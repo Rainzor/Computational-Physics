a18ed4d75dd46583eba56ce0012ab8285bf2b5d6
Rainzor <runze.wang02@outlook.com>
2022-09-18 00:05:11 +0800

    hw2 test down

--- hw1&2/hw1.py
+++ hw1&2/hw1.py
@@ -1,7 +1,37 @@
+#Using UTF-8 Code Python
+#Runze Wang
+#PB20020480
+
+
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+#Schrahe 16807类
+#随机输入初始值，反复调用rand(),得到随机数列
+class Schrage(object):
+    def __init__(self,seed = 42):
+        self.__seed = seed
+        self.__M = 2147483647
+        self.__a = 16807
 
-def uniformity(data, k = 10):
+    def __iter_rand(self, i_n):#随机数迭代
+        a = self.__a
+        m = self.__M
+        q = m//a
+        r = m%a
+        i_next = a * (i_n % q) - r * (i_n // q)
+        if i_next < 0:
+            return i_next + m
+        else:
+            return i_next
+
+    def rand(self):
+        self.__seed = self.__iter_rand(self.__seed)
+        return self.__seed/self.__M
+
+
+
+def uniformity(data, k = 10):#计算一致性
     lp = np.linspace(0,1,k+1)
     nk = [0] * k
     size = len(data)
@@ -20,7 +50,7 @@ def uniformity(data, k = 10):
         chi = chi + ((nk[i]-mk)**2)/mk
     return chi
 
-def covariance2(data, l=2):
+def covariance2(data, l=2):#计算相关性
     x = np.array(data[:-l])
     y = np.array(data[l:])
     d = np.array(data)
@@ -31,28 +61,10 @@ def covariance2(data, l=2):
 
     return (rcl - r1)/(r2 - r1)
 
+def makefile(data,filename):
+    df = pd.DataFrame({'data':data,})
+    df.to_excel(filename+'xlsx',sheet_name='sheet1', index=False)
 
-
-class Schrage(object):
-    def __init__(self,seed = 42):
-        self.__seed = seed
-        self.__M = 2147483647
-        self.__a = 16807
-
-    def __iter_rand(self, i_n):
-        a = self.__a
-        m = self.__M
-        q = m//a
-        r = m%a
-        i_next = a * (i_n % q) - r * (i_n // q)
-        if i_next < 0:
-            return i_next + m
-        else:
-            return i_next
-
-    def rand(self):
-        self.__seed = self.__iter_rand(self.__seed)
-        return self.__seed/self.__M
 
 if __name__ == "__main__":
     test = Schrage(seed = 42)
@@ -62,6 +74,7 @@ if __name__ == "__main__":
     x = data[:-1]
     y = data[1:]
 
+    #绘图，散点图
     plt.scatter(x,y, s=0.01, c='b')
     plt.show()
 
